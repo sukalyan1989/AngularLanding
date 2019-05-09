e01d1ba418783ccff0f855e34dde3cb7572f541e
sukalyan89 <sukalyan89@hotmail.com>
2019-05-09 18:55:33 +0530

    Design changes and tweaks

--- src/app/my-chart/my-chart.component.ts
+++ src/app/my-chart/my-chart.component.ts
@@ -3,19 +3,37 @@ import { Component, OnInit,Input } from '@angular/core';
 @Component({
   selector: 'my-chart',
   templateUrl: './my-chart.component.html',
-  styleUrls: ['./my-chart.component.css']
+  styleUrls: ['./my-chart.component.css'] 
 })
 export class MyChartComponent implements OnInit {
 
 @Input() ChartData;
+@Input()ChartName;
 public ChartLabels = ['Boys','Girls'];
 public ChartType='doughnut';
 public ChartLegend=false;
+public ChartOptions={
+  title:{
+  display:true,
+  text:this.ChartName==null?'True':this.ChartName
+}}
   
 
   constructor() { }
 
   ngOnInit() {
+
+  }
+  ngOnChange(){
+    console.log(this.ChartName)
+    this.ChartOptions={
+      title:{
+        display:true,
+        text:this.ChartName
+
+      }
+    }
+
   }
 
 }
